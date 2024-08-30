8613555e00f1ab31b3a740bce4f0008bbac320d5
emrea-works <dev@emreakyuz.works>
2024-08-30 02:41:51 -0300

    last minute provider update

--- packages/nextjs/components/ScaffoldEthAppWithProviders.tsx
+++ packages/nextjs/components/ScaffoldEthAppWithProviders.tsx
@@ -13,6 +13,15 @@ import { BlockieAvatar } from "~~/components/scaffold-eth";
 import { ProgressBar } from "~~/components/scaffold-eth/ProgressBar";
 import { useInitializeNativeCurrencyPrice } from "~~/hooks/scaffold-eth";
 import { wagmiConfig } from "~~/services/web3/wagmiConfig";
+/* */
+import {
+  ConnectProvider,
+  OKXConnector,
+  UnisatConnector,
+  BitgetConnector
+} from '@particle-network/btc-connectkit';
+import { Merlin } from '@particle-network/chains';
+
 
 const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
   useInitializeNativeCurrencyPrice();
@@ -47,16 +56,37 @@ export const ScaffoldEthAppWithProviders = ({ children }: { children: React.Reac
   }, []);
 
   return (
-    <WagmiProvider config={wagmiConfig}>
-      <QueryClientProvider client={queryClient}>
-        <ProgressBar />
-        <RainbowKitProvider
-          avatar={BlockieAvatar}
-          theme={mounted ? (isDarkMode ? darkTheme() : lightTheme()) : lightTheme()}
-        >
-          <ScaffoldEthApp>{children}</ScaffoldEthApp>
-        </RainbowKitProvider>
-      </QueryClientProvider>
-    </WagmiProvider>
+    <ConnectProvider
+      options={{
+        projectId: process.env.NEXT_PUBLIC_PROJECT_ID, // ---
+        clientKey: process.env.NEXT_PUBLIC_CLIENT_KEY, // Retrieved from https://dashboard.particle.network
+        appId: process.env.NEXT_PUBLIC_APP_ID, // ---
+        aaOptions: {
+          accountContracts: {
+            BTC: [
+              {
+                chainIds: [Merlin.id, MerlinTestnet.id], // The chain you'd like to use, Merlin in this case.
+                version: '1.0.0', // Keep this as 1.0.0 for now.
+              },
+            ],
+          },
+        },
+        walletOptions: {
+          visible: true, // Whether or not the embedded wallet modal (for controlling the smart account) is shown.
+        }
+      }}
+      // List of supported wallets.
+      connectors={[
+        new UnisatConnector(),
+        new OKXConnector(),
+        new BitgetConnector(),
+        new TokenPocketConnector(),
+        new BybitConnector(),
+        new WizzConnector(),
+        new XverseConnector(),
+      ]}
+    >
+      <ScaffoldEthApp>{children}</ScaffoldEthApp>
+    </ConnectProvider>
   );
 };
